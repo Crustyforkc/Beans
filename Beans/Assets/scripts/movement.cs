4c32820beb85369d924eac88e4f4f67028f08e6f
msolaro <70597109+msolaro@users.noreply.github.com>
2021-07-13 09:59:36 -0700

    thirdperson movement and camera
    made some thirdperson camera following and movement kinda boof

--- Beans/Assets/scripts/movement.cs
+++ Beans/Assets/scripts/movement.cs
@@ -23,26 +23,33 @@ public class movement : MonoBehaviour
        if(Input.GetKey("d"))
        {
        		rb.AddForce(forwardForce * Time.deltaTime,0,0);
+       		//go forward (z axis +)
        }
 
        if(Input.GetKey("a"))
        {
        		rb.AddForce(-forwardForce * Time.deltaTime,0,0);
+       		//go backward (z axis -)
        }
 
        if(Input.GetKey("w"))
        {
        		rb.AddForce(0,0,forwardForce * Time.deltaTime);
+       		//go right (x axis +)
        }
 
        if(Input.GetKey("s"))
        {
        		rb.AddForce(0,0, -forwardForce * Time.deltaTime);
+       		//go left (x axis -)
        }
 
        if(Input.GetKey("space"))
        {
        		rb.AddForce(0,forwardForce * Time.deltaTime,0);
+       		//go up (y axis +)
+       		//should probably put something to check when space is it and then blacklist it otherwise you can fly
+
        }
 
     }
